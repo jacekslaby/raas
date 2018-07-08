4d81158f1436936d778cffca7a56406a4cd55b58
Jacek Slaby <jacek.slaby@interia.pl>
2018-07-08 17:26:05 +0100

    Basic request is working - http://localhost:8080/v1/rawalarms?notificationIdentifier=sddd

---
+++ src/main/java/com/j9soft/poc/alarms/RaasApplication.java
@@ -0,0 +1,12 @@
+package com.j9soft.poc.alarms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaasApplication.class, args);
+    }
+}
